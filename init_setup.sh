d9a9dee8229e414bde7b056dd3b01aabac2cbffd
Ayush-Pujari-07 <ayush@hobglobin.com>
2025-03-31 18:10:23 +0530

    Refactor setup scripts and update dependencies in requirements.txt

--- init_setup.sh
+++ init_setup.sh
@@ -3,8 +3,8 @@ echo [$(date)]: "Creating conda env with python 3.11" # CHANGE THE VERSION BASE 
 conda create --prefix ./venv python=3.11 -y
 echo [$(date)]: "activate virtual env"
 source activate ./venv
-echo [$(date)]: "intalling dev requirements with latest version"
-pip install -r requirements_dev.txt --upgrade
-# echo [$(date)]: "intalling dev requirements"
-# pip install -r requirements_dev.txt
+echo [$(date)]: "Install UV for python"
+pip install uv
+echo [$(date)]: "installing dev requirements with latest version"
+uv pip install -r requirements.txt
 echo [$(date)]: "END"
